f239510780e1a12db467866a8f7943472a4a8ce7
black <joseb.twelve@gmail.com>
2022-06-01 14:56:08 -0500

    feat: added body of login_email page

--- lib/pages/login_email.dart
+++ lib/pages/login_email.dart
@@ -8,10 +8,74 @@ class LoginEmail extends StatefulWidget {
 }
 
 class _LoginEmailState extends State<LoginEmail> {
+  Widget textField({label}) {
+    return SizedBox(
+      width: 300,
+      child: TextFormField(
+        decoration: InputDecoration(
+          label: Text("$label"),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+          )
+        ),
+      ),
+    );
+  }
+
+  Widget btn({child, onPressed, color}) {
+    return Container(
+      width: 300,
+      margin: EdgeInsets.symmetric(vertical: 2.5, horizontal: 10),
+      child: ConstrainedBox(
+        constraints: BoxConstraints(minWidth: double.infinity, minHeight: 40),
+        child: ElevatedButton(
+          child: child,
+          onPressed: onPressed,
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(
+              color
+            ),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50),
+              )
+            )
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(title: Text("LoginEmail"))
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Center(
+                child: Image.asset(
+                  "assets/login_image.png",
+                  height: 200
+                )
+              ),
+              textField(label: "password"),
+              btn(
+                child: Text("Login"),
+                onPressed: _login,
+                color: Colors.blue
+              )
+            ]
+          )
+        )
+      )
     );
+  }
+
+  void _login() {
+    print("login");
   }
 }
