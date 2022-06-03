8dfcdced47776f6b20fd5e6506c0d44b9fc2f204
black <joseb.twelve@gmail.com>
2022-06-03 12:48:14 -0500

    feat: added veterinary_enable body

--- lib/pages/veterinary_enable.dart
+++ lib/pages/veterinary_enable.dart
@@ -10,9 +10,41 @@ class VeterinaryEnable extends StatefulWidget {
 class _VeterinaryEnableState extends State<VeterinaryEnable> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: Container(
-        color: Colors.blue
+        width: size.width,
+        height: size.height,
+        color: Colors.blue,
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              const Text(
+                "Seleccione veterinaria",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                )
+              ),
+              const SizedBox(height: 10),
+              Expanded(
+                child: ListView.builder(
+                  padding: EdgeInsets.zero,
+                  shrinkWrap: true,
+                  itemCount: 28,
+                  itemBuilder: (context, i) {
+                    return Container(
+                      margin: const EdgeInsets.all(5),
+                      child: Image.asset("assets/imagen 4.png")
+                    );
+                  },
+                ),
+              ),
+            ]
+          ),
+        )
       )
     );
   }
